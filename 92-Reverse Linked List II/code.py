cb30a0d4283c91a1f7e9a0d723933eaaa3e9d2a1
mustafa ashraf <mustafaashraf744@gmail.com>
2023-11-14 03:38:20 +0200

    Create code.py
    solution to the reverse liken list problem with time complexity O(n)

---
+++ 92-Reverse Linked List II/code.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/reverse-linked-list-ii/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        for i in range(left - 1):
+            prev = prev.next
+        curr = prev.next
+        for i in range(right - left):
+            next_node = curr.next
+            curr.next = next_node.next
+            next_node.next = prev.next
+            prev.next = next_node
+        return dummy.next
